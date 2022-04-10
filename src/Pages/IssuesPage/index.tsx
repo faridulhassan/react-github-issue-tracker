e75d4ff05201d029ffad139c3ef0d2e1633df7b2
LAPTOP-4BFHKCR3\Faridul <faridul@sender.net>
2022-04-10 18:49:44 +0600

    Added issues page

---
+++ src/Pages/IssuesPage/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import IssueList from "../../components/IssueList";
+
+const IssuesPage: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <IssueList />
+    </div>
+  );
+};
+export default IssuesPage;
