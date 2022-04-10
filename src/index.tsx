2617f8d6b0ac138de10f876f05a5c814f4c776dc
LAPTOP-4BFHKCR3\Faridul <faridul@sender.net>
2022-04-10 18:54:22 +0600

    Added necessary routers for the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import reportWebVitals from "./reportWebVitals";
+
+/*Styles*/
+import "./index.css";
+
+/*Components*/
+import App from "./App";
+import Header from "./components/Header";
+import Loader from "./components/Loader";
+
+/*Store*/
+import { store } from "./store";
+
+/*Pages*/
+const HomePage = React.lazy(() => import("./Pages/HomePage"));
+const IssuesPage = React.lazy(() => import("./Pages/IssuesPage"));
+const NotFoundPage = React.lazy(() => import("./Pages/NotFoundPage"));
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route
+              index
+              element={
+                <React.Suspense fallback={<Loader isPageType={true} />}>
+                  <HomePage />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path={"/issues"}
+              element={
+                <React.Suspense fallback={<Loader isPageType={true} />}>
+                  <IssuesPage />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path="*"
+              element={
+                <React.Suspense fallback={<Loader isPageType={true} />}>
+                  <NotFoundPage />
+                </React.Suspense>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
