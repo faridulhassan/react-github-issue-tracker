8ba7e58c16b4fe1dd7da28ca72d08650b98f44ea
LAPTOP-4BFHKCR3\Faridul <faridul@sender.net>
2022-04-10 18:53:59 +0600

    structured the app layout and imported router outlet also added a toaster for error message showing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Outlet } from "react-router-dom";
 
-function App() {
+/*Store*/
+import { RootState } from "./store";
+
+/*Components*/
+import Container from "./components/Container";
+import Toast from "./components/Toast";
+
+function App(): JSX.Element {
+  const errorMessage = useSelector(
+    (state: RootState) => state.issuesReducer.apiParams.errorMessage
+  );
   return (
     <div className="App">
-
+      <Container>
+        <Outlet />
+        <Toast show={!!errorMessage} message={errorMessage} />
+      </Container>
     </div>
   );
 }
